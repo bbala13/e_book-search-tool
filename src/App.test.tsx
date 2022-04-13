972f28279124b4bec702394c9d3e49d50490d7ba
Bran <bran.balachandran@gmail.com>
2022-04-13 11:02:43 -0400

    added: tests for app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-    const { getByText } = render(
+const renderComponent = () =>
+    render(
         <Provider store={store}>
             <App />
         </Provider>
     );
 
-    expect(getByText(/hello world/i)).toBeInTheDocument();
+describe('App', () => {
+    test('should render title', () => {
+        renderComponent();
+        expect(
+            screen.getByRole('heading', { level: 1, name: /book search tool/i })
+        ).toBeInTheDocument();
+    });
+
+    test('should render search bar', () => {
+        renderComponent();
+        const searchBarForm = screen.getByRole('search');
+        expect(searchBarForm).toBeInTheDocument();
+
+        expect(
+            within(searchBarForm).getByLabelText(/search/i)
+        ).toBeInTheDocument();
+
+        expect(
+            within(searchBarForm).getByRole('button', { name: /search/i })
+        ).toBeInTheDocument();
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-    return <div className="App">hello world</div>;
+    return (
+        <div className="App">
+            <h1>Book Search Tool</h1>
+
+            <form role="search">
+                <label htmlFor="search-bar">
+                    <span className="visually-hidden">Search</span>
+                </label>
+                <input type="text" id="search-bar" />
+                <button type="submit">
+                    <span className="visually-hidden">Submit Search</span>
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
